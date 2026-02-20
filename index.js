d18f1fbae093c1c445fc9a37a0909914d1741d7e
Lamine DIOUBATE <domanilamine96@gmail.com>
2026-02-19 22:26:03 -0500

    affichage du quote generer terminer besoin d'ajustement du style

--- index.js
+++ index.js
@@ -2,4 +2,22 @@
 let quotes = document.getElementById("randomQuote");
 let authors = document.getElementById("author");
 
+let btn = document.getElementById("getQuote");
+
 const URL = `https://api.quotable.io/random`;
+
+async function getQuotes() {
+	const response = await fetch(URL, {
+		method: "GET",
+	});
+
+	if (!response.ok) {
+		alert("un probleme est survenu veuillez ressayer SVP!!!");
+	} else {
+		let data = await response.json();
+		quotes.innerHTML = data.content;
+		authors.textContent = data.author;
+	}
+}
+
+btn.addEventListener("click", getQuotes);
